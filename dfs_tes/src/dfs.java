c5414b2201ae4983a0d794f243880d0dd4a7e2a2
dapit pio <dapit pio@dapitpio>
2013-04-27 03:36:32 +0700

    anyeong

---
+++ dfs_tes/src/dfs.java
@@ -0,0 +1,62 @@
+public class dfs {
+    private boolean[] marked;    // marked[v] = is there an s-v path?
+    private int[] edgeTo;        // edgeTo[v] = last edge on s-v path
+    private final int s;         // source vertex
+
+    public dfs(Graph G, int s) {
+        this.s = s;
+        edgeTo = new int[G.V()];
+        marked = new boolean[G.V()];
+        dfs(G, s);
+    }
+
+    // depth first search from v
+    private void dfs(Graph G, int v) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            }
+        }
+    }
+
+    // is there a path between s and v?
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    // return a path between s to v; null if no such path
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<Integer> path = new Stack<Integer>();
+        for (int x = v; x != s; x = edgeTo[x])
+            path.push(x);
+        path.push(s);
+        return path;
+    }
+
+    public static void main(String[] args) {
+    	In in = new In("C:/Documents and Settings/dapit pio/My Documents/phio/dfs/tinyDG.txt");
+        Graph G = new Graph(in);
+        int s = Integer.parseInt("0");
+        dfs dfs = new dfs(G, s);
+
+        for (int v = 0; v < G.V(); v++) {
+            if (dfs.hasPathTo(v)) {
+                StdOut.printf("%d to %d:  ", s, v);
+                for (int x : dfs.pathTo(v)) {
+                    if (x == s) StdOut.print(x);
+                    else        StdOut.print("-" + x);
+                }
+                StdOut.println();
+            }
+
+            else {
+                StdOut.printf("%d to %d:  not connected\n", s, v);
+            }
+
+        }
+    }
+
+}
